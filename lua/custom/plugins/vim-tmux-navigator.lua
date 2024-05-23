293fde4c9570ef362b48bb22c47c532997cff7e9
Magnus Larsen <magnushlarsen01@protonmail.com>
2024-05-23 15:51:43 +0200

    vim-tmux-navigator config

---
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        'christoomey/vim-tmux-navigator',
+    },
+    vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
+    vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
+    vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
+    vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
+}
