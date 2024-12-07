f1ebfaaf88af08bc175bdb460e8e0631013ae00a
Magnus Larsen <magnushlarsen01@protonmail.com>
2024-12-07 18:10:56 +0100

    add phpactor and fix config after upstream merge

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,12 +1,12 @@
-return {
-    {
-        'akinsho/bufferline.nvim',
-        version = '*',
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        opts = {
-            options = {
-                numbers = 'buffer_id',
-            },
-        },
-    },
-}
+-- return {
+--     {
+--         'akinsho/bufferline.nvim',
+--         version = '*',
+--         dependencies = 'nvim-tree/nvim-web-devicons',
+--         opts = {
+--             options = {
+--                 numbers = 'buffer_id',
+--             },
+--         },
+--     },
+-- }
